7aeddb00bce2109c66b87e258b82170071f9a664
eloinshuti <112521045+eloinshuti@users.noreply.github.com>
2022-12-15 16:48:35 -0700

    Chapter 21 Exercises

---
+++ Ch21 Exercises/src/WordOccurrence.java
@@ -0,0 +1,24 @@
+/* Eloi Nshuti
+ * 12/15/2022
+ * Programming Exercise 21-7
+ */
+
+public class WordOccurrence implements Comparable<WordOccurrence> {
+	Integer count;
+	String word;
+
+	public WordOccurrence(String word, Integer count) {
+		this.word = word;
+		this.count = count;
+	}
+
+	@Override
+	public int compareTo(WordOccurrence o) {
+		return o.count.compareTo(count);
+	}
+
+	 public String toString() {
+		return word + "    " + count;
+
+	 }
+  }
